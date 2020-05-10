749e5b2a9ccf60af65c65f41da4b5d1d0d4f2b7c
Elga Theresia <elga.theresia@hotmail.com>
2020-05-11 00:16:12 +0700

    update screen kalkulator

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((number) => {
     number.addEventListener("click", (event) => {
-        console.log(event.target.value)
+        updateScreen(event.target.value)
     })
 })
+
+const calculatorScreen = document.querySelector('.calculator-screen');
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+};
+
