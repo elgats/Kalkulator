630d4c2aa170ce73d5e66fc228529eb0c591c985
Elga Theresia <elga.theresia@hotmail.com>
2020-05-11 00:26:16 +0700

    variables for calculation
    Untuk kalkulasi: 2 angka dan 1 operator

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Steps:
+// 1. Ambil element button dengan class number.
+// 2. Tambah click event & jalankan function "updateScreen" saat tombol diklik.
+// 3. Perbarui atribut nilai dari element <input> di dalam calculator-screen dengan function "updateScreen".
+
+
 const numbers = document.querySelectorAll(".number");
 //console.log(numbers);
 
@@ -13,3 +19,5 @@ const updateScreen = (number) => {
     calculatorScreen.value = number
 };
 
+
+//Untuk kalkulasi: 2 angka dan 1 operator
